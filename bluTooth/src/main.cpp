7c87ab8c70e2da3d7b9794123b601af0529084e9
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2025-04-08 10:10:28 +1200

    methDetect

--- bluTooth/src/main.cpp
+++ bluTooth/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
+/**
+ * BLE Connection Life Cycle:
+ * Advertising: The peripheral device broadcasts its presence.
+ * Scanning: The central device searches for advertising devices.
+ * Connection Request: The central device initiates a connection request.
+ * Connection Established: The peripheral device accepts the connection.
+ * Data Exchange: The central and peripheral devices exchange data.
+ * Disconnection: The connection is terminated
+ * 
+ * Peripheral device is the ESP32
+ * Central device is the phone etc
+ * another way of saying this is the ESP is a server and your phone is the client
+ * 
+ * https://reference.arduino.cc/reference/en/libraries/arduinoble/
+ * 
+ * I use nRF Connect App on my phone to play with these settings
+**/
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// Bluetooth setup
