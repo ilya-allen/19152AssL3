25dd61fe4259ea1ce4b79d8c5f5cc717954e74d9
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2025-05-16 09:29:18 +1200

    littleFSADDED

--- websiteExample/src/main.cpp
+++ websiteExample/src/main.cpp
@@ -8,6 +8,7 @@
 const char SSID[] = "T8-Arduino";  
 const char PASSWORD[] = "T8-Arduino";
 
+// Create an async web server
 AsyncWebServer server(80);
 
 const byte LEDPIN = LED_BUILTIN;
@@ -59,4 +60,29 @@ void setup() {
   initWifi();
   initLittleFS();
   pinMode(LEDPIN, OUTPUT);
+
+  // Server.on defines a route handler. It awaits a request and tells your server what to do when a specific url is received
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // When the request is received, it sends a file. The file sent is the index.html which is the main file. The false means not to store any cache
+    request->send(LittleFS, "/index.html", "text/html", false, processor); 
+  });
+  // Make everyfile easily requestable by LittleFS web server, eg. style and script.
+  server.serveStatic("/", LittleFS, "/");
+  // Same route handler which this time when it receives a request , not only sends the index.html main file but also turns on the LED
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+    digitalWrite(LEDPIN, HIGH);
+    request->send(LittleFS, "/index.html", "text/html", false, processor);
+  });
+
+  // Same but turns the lef off  
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
+    digitalWrite(LEDPIN, LOW);
+    request->send(LittleFS, "/index.html", "text/html", false, processor);
+  });
+
+  //turn on the server
+  server.begin();
+
 }
+
+void loop() {}
