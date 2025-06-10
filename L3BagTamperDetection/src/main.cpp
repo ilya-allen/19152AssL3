99bb209f3d5a350a27af978e17a914b2a8ca565e
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2025-06-11 11:46:55 +1200

    updated with HTML file

--- L3BagTamperDetection/src/main.cpp
+++ L3BagTamperDetection/src/main.cpp
@@ -5,6 +5,9 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_Sensor.h>
 
+//Includes the document storing HTML variables
+#include 'arduinoHTML.h'
+
 // Sensors Pins
 const byte LEDPIN = LED_BUILTIN;
 const byte SENSORPIN = A5;
