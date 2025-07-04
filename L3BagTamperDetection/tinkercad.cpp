88a7f4adbc7de75d89ef93cbb8dd96bbe14dcddb
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2025-07-04 16:35:36 +1200

    Update tinkercad.cpp
    two new functions

--- L3BagTamperDetection/tinkercad.cpp
+++ L3BagTamperDetection/tinkercad.cpp
@@ -5,22 +5,55 @@ const byte PRESSENSOR = A1;
 const byte DEFAULTTEMP = 35;
 
 unsigned long lastCallbackTime = 0;
-int chosenDelay;
+int chosenDelay = 5 * 60;
 byte chosenTemp = DEFAULTTEMP;
 
+void userPref(byte &chosenTemp) {
+  Serial.print("Default Temp: ");
+  Serial.println(chosenTemp);
+  Serial.println("Choose Your Preffered Temperature: ");
+  // Wait for user input
+  while(Serial.available() == 0) {
+     
+  }
+  chosenTemp = Serial.parseInt(); // Read user input
+  Serial.print("Your Chosen Temp: ");
+  Serial.println(chosenTemp);
+  Serial.println("Now Choose Your Preffered delay between temperature checks in minutes (shorter time between checks can result in more power usage): ");
+  while(Serial.available() == 0) {
+     
+  }
+  chosenDelay = Serial.parseInt() * 60;
+  Serial.print("Your Chosen Delay: ");
+  Serial.print(chosenDelay / 60);
+  Serial.println(" Minutes");
+}
 
-void setup()
-{
+void tempCheck(byte val) {
+  if(val >= chosenTemp + 1) {
+    digitalWrite(HEATBULB, LOW);
+  } else if (val <= chosenTemp + 1) {
+  	digitalWrite(HEATBULB, HIGH);
+  }	else {
+  	digitalWrite(HEATBULB, LOW);
+  }
+}
+
+void setup() {
   Serial.begin(9600);
+  Serial.parseInt();
   pinMode(HEATBULB, OUTPUT);
   pinMode(TEMPSENSOR, INPUT);
   pinMode(PRESSENSOR, INPUT);
+  userPref(chosenTemp);
 }
 
-void loop()
-{
-  int sensorVal = analogRead(PRESSENSOR);
-  Serial.println(sensorVal);
-
+void loop() {
+  byte sensorVal = analogRead(TEMPSENSOR);
+  
+  if(millis() - lastCallbackTime >= chosenDelay) {
+  	tempCheck(sensorVal);
+  }
+  
   
 }
