e00f223de014673cf9a4289fbfe3b4ffa066a171
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2025-07-08 16:39:59 +1200

    Update tinkercad.cpp

--- L3BagTamperDetection/tinkercad.cpp
+++ L3BagTamperDetection/tinkercad.cpp
@@ -1,44 +1,61 @@
 // C++ code
+// Code written by Il'ya Allen
+
+// Constant (non changing) Variables
 const byte HEATBULB = 3;
 const byte TEMPSENSOR = A0; 
 const byte PRESSENSOR = A1;
 const byte DEFAULTTEMP = 35;
 
+// Changing Variables, all number based
 unsigned long lastCallbackTime = 0;
 int chosenDelay = 5 * 60;
 byte chosenTemp = DEFAULTTEMP;
 
+
+// Function that calls upon user preferences, when the user
+// starts up the program, the user will be asked for their
+// preffered tempereature and delay between checks to save power.
 void userPref(byte &chosenTemp) {
+  // Prints questions into the serial for the user to read.
   Serial.print("Default Temp: ");
   Serial.println(chosenTemp);
   Serial.println("Choose Your Preffered Temperature: ");
-  // Wait for user input
+  // Wait for user input before continuing with next tasks
   while(Serial.available() == 0) {
      
   }
   chosenTemp = Serial.parseInt(); // Read user input
   Serial.print("Your Chosen Temp: ");
   Serial.println(chosenTemp);
+  
+  // Same as aboved but with changed text and variable names
   Serial.println("Now Choose Your Preffered delay between temperature checks in minutes (shorter time between checks can result in more power usage): ");
   while(Serial.available() == 0) {
      
   }
-  chosenDelay = Serial.parseInt() * 60;
+  chosenDelay = Serial.parseFloat() * 60;
   Serial.print("Your Chosen Delay: ");
   Serial.print(chosenDelay / 60);
   Serial.println(" Minutes");
 }
 
+
+// This function is called constantly after however many seconds
+// the user determined at the start of the program. 
 void tempCheck(byte val) {
+  // If statements determining whether to increase or decrease heat
   if(val >= chosenTemp + 1) {
     digitalWrite(HEATBULB, LOW);
-  } else if (val <= chosenTemp + 1) {
+  } else if (val <= chosenTemp - 1) {
   	digitalWrite(HEATBULB, HIGH);
   }	else {
   	digitalWrite(HEATBULB, LOW);
   }
 }
 
+// Setup calls all functions and completes all tasks that need 
+// doing before void loop is reached.
 void setup() {
   Serial.begin(9600);
   Serial.parseInt();
@@ -48,10 +65,15 @@ void setup() {
   userPref(chosenTemp);
 }
 
+
+// Main looping block of code. These functions are called
+// constantly and non-stop.
 void loop() {
-  byte sensorVal = analogRead(TEMPSENSOR);
-  
-  if(millis() - lastCallbackTime >= chosenDelay) {
+  unsigned long currentMillis = millis();
+  byte sensorVal = map(((analogRead(TEMPSENSOR) - 20) * 3.04), 0, 1023, -40, 125); // Changes the values into easily outputted options
+  //
+  if(currentMillis - lastCallbackTime >= chosenDelay * 1000) {
+    lastCallbackTime = millis();
   	tempCheck(sensorVal);
   }
   
